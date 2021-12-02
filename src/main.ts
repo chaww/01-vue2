2fc6a7c0c91facdee5de8c3cc5092122ba9c21e2
Chatchawan Wongchai <81462904+chaww@users.noreply.github.com>
2021-12-02 07:17:08 +0700

    first commit

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,19 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
+import Hero from './components/Hero.vue'
+Vue.component("Hero", Hero);
+Vue.prototype.$hero = {
+  items: {},
+  inUse: {},
+};
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,69 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Home from "../views/Home.vue";
+import AppBar from '../components/appBar/appBar.vue'
 
 Vue.use(VueRouter);
+
+const mainComponents = {
+  appBar: AppBar
+}
 
 const routes: Array<RouteConfig> = [
   {
     path: "/",
     name: "Home",
     component: Home,
-  },
-  {
+    components: {
+      default: Home,
+      ...mainComponents,
+    },
+  }
+  , {
     path: "/about",
-    name: "About",
+    name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
   },
+  {
+    path: "/list",
+    name: "list",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/List.vue")
+  },
+  {
+    path: "/player/:id",
+    name: "player",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Player.vue")
+  },
+  {
+    path: "/image",
+    name: "image",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Image.vue")
+  },
+  {
+    path: "/grid",
+    name: "grid",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/GridList.vue")
+  }
 ];
 
 const router = new VueRouter({
